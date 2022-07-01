b510e374270373f37dfe5da9b68560f43e5a4c6d
Vishal <mistryvishal1994@gmail.com>
2022-07-01 12:46:35 +0530

    UT Added

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.17
 require (
 	github.com/doug-martin/goqu/v9 v9.16.0
 	github.com/gouniverse/uid v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.7
 )
---
+++ store_test.go
@@ -0,0 +1,222 @@
+package settingstore
+
+import (
+	"os"
+	"testing"
+
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB(filepath string) *sql.DB {
+	os.Remove(filepath) // remove database
+	dsn := filepath + "?parseTime=true"
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func InitStore() *Store {
+	db := InitDB("test_settingstore.db")
+	return &Store{
+		settingsTableName:  "test_settingsTableName.db",
+		dbDriverName:       "sql",
+		db:                 db,
+		debug:              false,
+		automigrateEnabled: false,
+	}
+}
+
+func TestWithAutoMigrate(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+
+	s := Store{
+		settingsTableName:  "test_settingsTableName.db",
+		dbDriverName:       "sql",
+		db:                 db,
+		debug:              false,
+		automigrateEnabled: false,
+	}
+
+	f := WithAutoMigrate(true)
+	f(&s)
+
+	if s.automigrateEnabled != true {
+		t.Fatalf("automigrateEnabled: Expected [true] received [%v]", s.automigrateEnabled)
+	}
+
+	s = Store{
+		settingsTableName:  "test_settingsTableName.db",
+		dbDriverName:       "sql",
+		db:                 db,
+		debug:              false,
+		automigrateEnabled: true,
+	}
+
+	f = WithAutoMigrate(false)
+	f(&s)
+
+	if s.automigrateEnabled == true {
+		t.Fatalf("automigrateEnabled: Expected [true] received [%v]", s.automigrateEnabled)
+	}
+}
+
+func TestWithDb(t *testing.T) {
+	db := InitDB("test")
+	s := Store{
+		settingsTableName:  "test_settingsTableName.db",
+		dbDriverName:       "sql",
+		db:                 db,
+		debug:              false,
+		automigrateEnabled: true,
+	}
+
+	f := WithDb(db)
+	f(&s)
+
+	if s.db == nil {
+		t.Fatalf("DB: Expected Initialized DB, received [%v]", s.db)
+	}
+
+}
+
+func TestWithTableName(t *testing.T) {
+	s := Store{
+		settingsTableName:  "test_settingsTableName.db",
+		dbDriverName:       "sql",
+		db:                 nil,
+		debug:              false,
+		automigrateEnabled: true,
+	}
+	table_name := "Table1"
+	f := WithTableName(table_name)
+	f(&s)
+	if s.settingsTableName != table_name {
+		t.Fatalf("Expected logTableName [%v], received [%v]", table_name, s.settingsTableName)
+	}
+	table_name = "Table2"
+	f = WithTableName(table_name)
+	f(&s)
+	if s.settingsTableName != table_name {
+		t.Fatalf("Expected logTableName [%v], received [%v]", table_name, s.settingsTableName)
+	}
+}
+
+func Test_Store_AutoMigrate(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+
+	s, _ := NewStore(WithDb(db), WithTableName("log_with_automigrate"), WithAutoMigrate(true))
+
+	s.AutoMigrate()
+
+	if s.settingsTableName != "log_with_automigrate" {
+		t.Fatalf("Expected logTableName [log_with_automigrate] received [%v]", s.settingsTableName)
+	}
+	if s.db == nil {
+		t.Fatalf("DB Init Failure")
+	}
+	if s.automigrateEnabled != true {
+		t.Fatalf("Failure:  WithAutoMigrate")
+	}
+}
+
+func Test_Store_Set(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+	s, _ := NewStore(WithDb(db), WithTableName("log_with_automigrate"), WithAutoMigrate(true))
+
+	key := "1234z"
+	val := "123zx"
+	ok, _ := s.Set(key, val)
+
+	if !ok {
+		t.Fatalf("Failure: Set")
+	}
+}
+
+func Test_Store_SetJSON(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+	s, _ := NewStore(WithDb(db), WithTableName("log_with_automigrate"), WithAutoMigrate(true))
+
+	key := "1234z"
+	val := `{"a" : "b", "c" : "d"}`
+	ok, _ := s.SetJSON(key, val)
+
+	if !ok {
+		t.Fatalf("Failure: Set")
+	}
+}
+
+func Test_Store_Remove(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+	s, _ := NewStore(WithDb(db), WithTableName("log_with_automigrate"), WithAutoMigrate(true))
+
+	key := "1234z"
+	val := "123zx"
+	ok, _ := s.Set(key, val)
+
+	if !ok {
+		t.Fatalf("Failure: Set")
+	}
+
+	s.Remove(key)
+	ret := s.Get(key, "default")
+	if ret != "default" {
+		t.Fatalf("Unable to delete!!! Entry Persists")
+	}
+}
+
+func Test_Store_Get(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+	s, _ := NewStore(WithDb(db), WithTableName("log_with_automigrate"), WithAutoMigrate(true))
+
+	key := "1234z"
+	val := "123zx"
+	ok, _ := s.Set(key, val)
+
+	if !ok {
+		t.Fatalf("Failure: Set")
+	}
+
+	ret := s.Get(key, "default")
+	if ret != val {
+		t.Fatalf("Unable to Get: Expected [%v] Received [%v]", val, ret)
+	}
+}
+
+func Test_Store_FindByKey(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+	s, _ := NewStore(WithDb(db), WithTableName("log_with_automigrate"), WithAutoMigrate(true))
+
+	key := "1234z"
+	val := "123zx"
+	ok, _ := s.Set(key, val)
+
+	if !ok {
+		t.Fatalf("Failure: Set")
+	}
+	meta := s.FindByKey(key)
+	if meta == nil {
+		t.Fatalf("NIL Record Received")
+	}
+}
+
+func Test_Store_GetJSON(t *testing.T) {
+	db := InitDB("test_settingsTableName.db")
+	s, _ := NewStore(WithDb(db), WithTableName("log_with_automigrate"), WithAutoMigrate(true))
+
+	key := "1234z"
+	val := `{"a" : "b", "c" : "d"}`
+	ok, _ := s.SetJSON(key, val)
+
+	if !ok {
+		t.Fatalf("Failure: Set")
+	}
+	ret := s.GetJSON(key, nil)
+	if ret == nil {
+		t.Fatalf("Failure getting JSON value")
+	}
+}
