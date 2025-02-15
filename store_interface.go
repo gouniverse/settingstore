3824a2868dc771f099ac057b5cd2e4dcf3a0d6ac
Sinevia <info@sinevia.com>
2025-02-15 09:15:05 +0000

    updated interface

--- store_interface.go
+++ store_interface.go
@@ -18,4 +18,15 @@ type StoreInterface interface {
 	SettingSoftDelete(ctx context.Context, setting SettingInterface) error
 	SettingSoftDeleteByID(ctx context.Context, settingID string) error
 	SettingUpdate(ctx context.Context, setting SettingInterface) error
+
+	Delete(ctx context.Context, settingKey string) error
+	Get(ctx context.Context, settingKey string, valueDefault string) (string, error)
+	GetAny(ctx context.Context, key string, valueDefault any) (any, error)
+	GetMap(ctx context.Context, key string, valueDefault map[string]any) (map[string]any, error)
+	Has(ctx context.Context, settingKey string) (bool, error)
+	MergeMap(ctx context.Context, key string, mergeMap map[string]any) error
+	Set(ctx context.Context, settingKey string, value string) error
+	SetAny(ctx context.Context, key string, value interface{}, seconds int64) error
+	SetMap(ctx context.Context, key string, value map[string]any) error
+	SettingDeleteByKey(ctx context.Context, settingKey string) error
 }
